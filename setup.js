3ac1de5648870fc112d8e6b990e0ed533c64a8d6
theduykh <theduykh@gmail.com>
2020-01-15 14:17:07 +0700

    add LOG_LENGTH

--- setup.js
+++ setup.js
@@ -3,6 +3,7 @@ global.expect = require('chai').expect;
 
 global.HOST = process.env.HOST ? process.env.HOST : 'jsonplaceholder.typicode.com';
 global.PROTOCOL = process.env.PROTOCOL ? process.env.PROTOCOL : 'https';
+global.LOG_LENGTH = process.env.LOG_LENGTH ? process.env.LOG_LENGTH : 1000;
 
 const logger = winston.createLogger({
   level: 'info',
